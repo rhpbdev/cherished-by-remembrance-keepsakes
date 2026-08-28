120ee562f579e39c82f0e300276d2113b82b08a8
Michael Scarano <accounts@rockyheights.com>
2026-08-28 17:04:33 -0500

    fix(drizzle): changed drizzle config and db.ts setup - note: needs unpooled env db var

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from "drizzle-kit";
-import "dotenv/config";
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+if (!process.env.DATABASE_URL_UNPOOLED) {
+  throw new Error('DATABASE_URL_UNPOOLED is not set in the .env file');
+}
 
 export default defineConfig({
   schema: "./src/drizzle/schema.ts",
   out: "./src/drizzle/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL_UNPOOLED,
   },
 });
--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,21 +1,10 @@
-import { Pool, neonConfig } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-serverless";
-import "dotenv/config";
-import * as schema from "./schema";
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
+import * as schema from './schema';
 
-neonConfig.webSocketConstructor = WebSocket;
-// Required for PlanetScale Postgres connections
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set in the .env file');
+}
 
-neonConfig.pipelineConnect = false;
-neonConfig.wsProxy = (host, port) => `${host}/v2?address=${host}:${port}`;
-
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-
-// Export the database connection and schema for use in other parts of the application
-export const db = drizzle({ client: pool, schema });
-
-// HTTP Connection example (uncomment to use):
-// import { drizzle } from 'drizzle-orm/neon-http';
-// import * as schema from './schema';
-
-// export const db = drizzle(process.env.DATABASE_URL as string, { schema });
+const sql = neon(process.env.DATABASE_URL);
+export const db = drizzle(sql, { schema });
